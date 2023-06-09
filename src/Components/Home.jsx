c1827abd8c6746fb8bff004e35f7f18703b54650
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2023-06-09 06:32:15 +0530

    add svg to home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import SvgComponent from "./SubComponents/SvgComponent";
 
 function Home() {
   return (
     <div className="home">
       <div className="hero">
         <div className="svg">
-          
+          <SvgComponent />
         </div>
       </div>
     </div>
