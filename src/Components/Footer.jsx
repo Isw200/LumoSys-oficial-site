b5c47e22c5424a0d134e8f53b40c493d315f68c0
DinithFdo <dinithofficial01@gmail.com>
2023-06-13 02:49:17 +0530

    Adds the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import bgImg from "../Assets/Backgrounds/footerBackground.png";
+import logo from "../Assets/Logo/logo.png";
+import "../Styles/Footer.css";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="Footer">
+      <div className="mainContainer">
+        <div
+          className="bgImgContainer"
+          style={{
+            backgroundImage: `url(${bgImg})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="main">
+            <div className="topMain">
+              <div className="columns">
+                <img src={logo} alt="" />
+                <p>
+                  Our skilled team illuminates businesses with tailored software
+                  solutions, staying ahead in a digital landscape. We understand
+                  unique requirements and possess expertise to create impactful
+                  results.
+                </p>
+              </div>
+              <div className="columns">
+                <h3>About</h3>
+              </div>
+              <div className="columns">
+                <h3>Resources</h3>
+              </div>
+              <div className="columns">
+                <h3>Connect with us</h3>
+              </div>
+            </div>
+            <div className="bar"></div>
+            <div className="bottomMain"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
