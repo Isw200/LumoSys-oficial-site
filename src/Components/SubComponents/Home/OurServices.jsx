afc872fe26994c29e613e9b61d86425eb5cf2d52
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2023-06-25 23:53:48 +0530

    add oue services slider

--- src/Components/SubComponents/Home/OurServices.jsx
+++ src/Components/SubComponents/Home/OurServices.jsx
@@ -1,14 +1,31 @@
-import CardSlider from "./CardSlider";
 import "../../../Styles/OurServices.css";
+import Carousel from "react-elastic-carousel";
+import Item from "./Item";
+import React, { useState } from "react";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 },
+];
 
 function OurServices() {
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+
   return (
     <div className="OurServices">
-      <div className="mainContainer">
-        <div className="descContainer">
-          <h1>Our Services</h1>
-        </div>
-        <CardSlider />
+      <div className="titles">
+        <h6>Our Services</h6>
+        <h4>Specialized IT Services Designed</h4>
+        <h5>for Your Industry's Demands</h5>
+      </div>
+      <div className="carousel-wrapper">
+        <Carousel breakPoints={breakPoints}>
+          {items.map((item) => (
+            <Item key={item}>{item}</Item>
+          ))}
+        </Carousel>
       </div>
     </div>
   );
