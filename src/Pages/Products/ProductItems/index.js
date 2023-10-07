a539663c569742ee2dda5238e7ec3deca0230e45
isuruariyarathna2k00@gmail.com <isuruariyarathna2k00@gmail.com>
2023-10-07 21:21:56 +0530

    done products page

---
+++ src/Pages/Products/ProductItems/index.js
@@ -0,0 +1,408 @@
+import React from 'react'
+
+// Carousel
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import './style.scss'
+
+const reviewresponsive = {
+    superLargeDesktop: {
+        breakpoint: { max: 4000, min: 3000 },
+        items: 2,
+    },
+    desktop: {
+        breakpoint: { max: 3000, min: 1450 },
+        items: 2,
+    },
+    desktopMini: {
+        breakpoint: { max: 1450, min: 1024 },
+        items: 2,
+    },
+    tablet: {
+        breakpoint: { max: 1024, min: 768 },
+        items: 2,
+    },
+    mobile: {
+        breakpoint: { max: 768, min: 0 },
+        items: 1,
+    },
+}
+
+const LumoLearn = () => {
+    return (
+        <div className='lumo-learn'>
+            <div className='lumo-learn-top'>
+                <div className='lumo-learn-top-left'>
+                    <h1>
+                        Lumo<span>Learn</span>
+                    </h1>
+                    <h2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eu lorem et ultricies.
+                    </p>
+                    <button>
+                        Learn More
+                    </button>
+                </div>
+                <div className='lumo-learn-top-right'>
+                    <img src={require('./Assets/lumolap.png')} alt=''></img>
+                </div>
+            </div>
+
+            <div className='lumo-learn-features'>
+                <div className='features-top'>
+                    <h1>
+                        Main Features of LumoLearn
+                    </h1>
+                    <h2>
+                        Manke your classes more efficient with LumoLearn
+                    </h2>
+                </div>
+
+                <div className='section-four-cards'>
+
+                    <div className='card' >
+                        <div className='card-image img3'>
+                            <img src={require('./Assets/Features/dashboard.png')} alt='card-img ' />
+                        </div>
+                        <div className='card-content'>
+                            <h1>
+                                Dashboard Hub
+                            </h1>
+                            <h2>
+                                Centralized class management at your fingertips.
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className='card' >
+                        <div className='card-image img3'>
+                            <img src={require('./Assets/Features/rec.png')} alt='card-img ' />
+                        </div>
+                        <div className='card-content'>
+                            <h1>
+                                Capture & Replay
+                            </h1>
+                            <h2>
+                                Seamless recording and playback of class sessions.
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className='card' >
+                        <div className='card-image img3'>
+                            <img src={require('./Assets/Features/folder.png')} alt='card-img ' />
+                        </div>
+                        <div className='card-content'>
+                            <h1>
+                                Resource Repository
+                            </h1>
+                            <h2>
+                                Easy access to class materials and resources.
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className='card' >
+                        <div className='card-image img3'>
+                            <img src={require('./Assets/Features/exam.png')} alt='card-img ' />
+                        </div>
+                        <div className='card-content'>
+                            <h1>
+                                Rapid Assessments
+                            </h1>
+                            <h2>
+                                Efficient testing to gauge understanding.
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className='card' >
+                        <div className='card-image img3'>
+                            <img src={require('./Assets/Features/payments.png')} alt='card-img ' />
+                        </div>
+                        <div className='card-content'>
+                            <h1>
+                                Payment Portal
+                            </h1>
+                            <h2>
+                                Simplified class fee management.
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className='card' >
+                        <div className='card-image img3'>
+                            <img src={require('./Assets/Features/calendar.png')} alt='card-img ' />
+                        </div>
+                        <div className='card-content'>
+                            <h1>
+                                Schedule Sync
+                            </h1>
+                            <h2>
+                                Effortless class scheduling and organization.
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className='card' >
+                        <div className='card-image img3'>
+                            <img src={require('./Assets/Features/goal.png')} alt='card-img ' />
+                        </div>
+                        <div className='card-content'>
+                            <h1>
+                                Progress Insights
+                            </h1>
+                            <h2>
+                                Monitor student development in real-time.
+                            </h2>
+                        </div>
+                    </div>
+
+                    <div className='card' >
+                        <div className='card-image img3'>
+                            <img src={require('./Assets/Features/report.png')} alt='card-img ' />
+                        </div>
+                        <div className='card-content'>
+                            <h1>
+                                Comprehensive Profiles
+                            </h1>
+                            <h2>
+                                Detailed student records for informed decisions.
+                            </h2>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className='why-lumolearn'>
+                <div className='why-lumolearn-left'>
+                    <h1>
+                        Why Lumo<span>Learn</span>?
+                    </h1>
+                    <h2>
+                        Elevate education with our feature-packed LMS, designed to simplify teaching, enhance learning, and empower educators and students alike.
+                    </h2>
+                    <button>
+                        Get Started
+                    </button>
+                </div>
+                <div className='why-lumolearn-right'>
+                    <img src={require('./Assets/pic1.png')} alt=''></img>
+                </div>
+            </div>
+
+            <div className='what-they-say'>
+                <div className='what-they-say-top'>
+                    <h1>
+                        What they say about us
+                    </h1>
+                    <h2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </h2>
+                </div>
+                <div className="review-slider" data-aos="zoom-in">
+                    <Carousel
+                        responsive={reviewresponsive}
+                        infinite={true}
+                        autoPlay={true}
+                        showDots={true}
+                        autoPlaySpeed={3000}
+                        containerClass="carousel-container"
+                        removeArrowOnDeviceType={[
+                            "tablet",
+                            "mobile",
+                            "desktop",
+                            "superLargeDesktop",
+                            "desktopMini",
+                        ]}
+                        dotListClass="custom-dot-list-style"
+                        activeDotClass="custom-dot-active"
+                    >
+
+                        <div className="card">
+                            <div className='card-image'>
+                                <img
+                                    src={require('./Assets/reviews/1.png')}
+                                    alt='1'
+                                />
+                            </div>
+                            <div className='card-content'>
+                                <h4>
+                                    User Name
+                                </h4>
+                                <h3>
+                                    CEO of Company
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.
+                                    adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="card">
+                            <div className='card-image'>
+                                <img
+                                    src={require('./Assets/reviews/1.png')}
+                                    alt='1'
+                                />
+                            </div>
+                            <div className='card-content'>
+                                <h4>
+                                    User Name
+                                </h4>
+                                <h3>
+                                    CEO of Company
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.
+                                    adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="card">
+                            <div className='card-image'>
+                                <img
+                                    src={require('./Assets/reviews/1.png')}
+                                    alt='1'
+                                />
+                            </div>
+                            <div className='card-content'>
+                                <h4>
+                                    User Name
+                                </h4>
+                                <h3>
+                                    CEO of Company
+                                </h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.
+                                    adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quas.
+                                </p>
+                            </div>
+                        </div>
+                    </Carousel>
+                </div>
+            </div>
+
+            <div className='pricing'>
+                <div className='pricing-top'>
+                    <h1>
+                        Pricing
+                    </h1>
+                    <h2>
+                        Bright up your classes with LumoLearn.
+                    </h2>
+                </div>
+
+                <div className='pricing-cards'>
+                    <div className='card card1'>
+                        <h1>
+                            Lumo<span className='learn'>Learn</span><span className='suf'> Lite</span>
+                        </h1>
+                        <ul>
+                            <li>1 Class</li>
+                            <li>Up to 150 students</li>
+                            <li>Video recordings</li>
+                            <li>Class materials</li>
+                            <li>Manage Students</li>
+                            <li>Overview Video</li>
+                            <li>Overview Content</li>
+                        </ul>
+                        <button>
+                            Get Started
+                        </button>
+                    </div>
+
+                    <div className='card card2'>
+                        <h1>
+                            Lumo<span className='learn'>Learn</span><span className='suf'> Plus</span>
+                        </h1>
+                        <ul>
+                            {/* feature */}
+                            <li>
+                                <li>Up to 5 Classes</li>
+                                <li>Up to 500 students</li>
+                                <li>Video recordings</li>
+                                <li>Class materials</li>
+                                <li>Manage Students</li>
+                                <li>Overview Video</li>
+                                <li>Overview Content</li>
+                                <li>Quick Tests</li>
+                                <li>Class Payments</li>
+                            </li>
+                        </ul>
+                        <button>
+                            Get Started
+                        </button>
+                    </div>
+
+                    <div className='card card3'>
+                        <h1>
+                            Lumo<span className='learn'>Learn</span><span className='suf'> Pro</span>
+                        </h1>
+                        <ul>
+                            <li>
+                                <li>Up to 10 Classes</li>
+                                <li>1000+ students</li>
+                                <li>Video recordings</li>
+                                <li>Class materials</li>
+                                <li>Manage Students</li>
+                                <li>Overview Video</li>
+                                <li>Overview Content</li>
+                                <li>Quick Tests</li>
+                                <li>Class Payments</li>
+                                <li>Class Scheduling</li>
+                                <li>Student's Progress</li>
+                                <li>Students Forums</li>
+                                <li>Chat Bot</li>
+                                <li>Class Reports to Parents</li>
+                            </li>
+                        </ul>
+                        <button>
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+                <div className='pricing-bottom'>
+                    <h4>
+                        Not found what you are looking for?
+                    </h4>
+                    <h2>
+                        Don't worry, we can customize a plan for you.
+                    </h2>
+                    <button>
+                        Contact Us
+                    </button>
+                </div>
+            </div>
+
+            <div className='lumolearn-getstated'>
+                <div className='lumolearn-getstated-left'>
+                    <h1>
+                        Get Started with LumoLearn
+                    </h1>
+                    <h2>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </h2>
+                    <button>
+                        Get Started
+                    </button>
+                </div>
+                <div className='lumolearn-getstated-right'>
+                    <div>
+                        <img src={require('./Assets/getintouch.png')} alt=''></img>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default LumoLearn
--- src/Pages/Products/index.js
+++ src/Pages/Products/index.js
@@ -1,8 +1,51 @@
 import React from 'react'
 
+import './style.scss'
+
 const Products = () => {
     return (
-        <div>Products</div>
+        <div className='our-products'>
+            <div className='products-top'>
+                <div className='products-top-left'>
+                    <h1>Our Products</h1>
+                    <h2>
+                        We provide the best quality products for you.
+                    </h2>
+                    <p>Our products are made with the best quality materials and are available in a variety of colors and sizes. Custom orders are available upon request. Please contact us for more information.
+                    </p>
+
+                    <div className='products-top-left-buttons'>
+                        <button
+                            onClick={() => {
+                                document.getElementById('product').scrollIntoView({ behavior: 'smooth' })
+                            }}
+                        >Learn More</button>
+                    </div>
+                </div>
+                <div className='products-top-right'>
+                    <img src={require('./Assets/amico.png')} alt=''></img>
+                </div>
+            </div>
+
+            <div className='products-bottom' id="product">
+                <div className='products-bottom-product'>
+                    <div className='product-image'>
+                    </div>
+                    <div className='products-bottom-product-info'>
+                        <h3>Lumo<span>Learn</span></h3>
+                        <h2>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        </h2>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eu lorem et ultricies. In porta lorem at dui semper porttitor. Nullam quis cursus dui.
+                        </p>
+                        <a href='/products/lumolearn'>
+                            Learn More
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
